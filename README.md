d313aecba3ca5abb82c61113426ffa971c195370
Hector <hectorbertucat@gmail.com>
2019-05-07 13:15:42 +0200

    Story 2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# spaceinvaders
+README.md (markdown)
+#Semaine du 29 avril au 5 mai
+##Description du travail effectué 
+Implémentation des deux premiers test. Problème lors de l'exécution d'Eclipse d'ou le retard. J'ai du réinstaller complétement le logiciel. 
+#Diagramme de classes 
+#Nuage de mots 
+------------------------------------------
--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -2,26 +2,37 @@ package fr.unilim.iut.spaceinvaders;
   
     public class SpaceInvaders {
 
-	    int longueur;
-	    int hauteur;
+    	int longueur;
+    	int hauteur;
+    	Vaisseau vaisseau;
 
-	    public SpaceInvaders(int longueur, int hauteur) {
-		   this.longueur = longueur;
-		   this.hauteur = hauteur;
-	   }
+    	//... Code déjà écrit
 	    
-	    @Override
-		public String toString() {
-			StringBuilder espaceDeJeu = new StringBuilder();
-			for (int i = 0; i < hauteur; i++) {
-				for (int j = 0; j < longueur; j++) {
-					espaceDeJeu.append('.');
-				}
-				espaceDeJeu.append('\n');
-			}
-			return espaceDeJeu.toString();
+    	public SpaceInvaders(int longueur, int hauteur) {
+ 		   this.longueur = longueur;
+ 		   this.hauteur = hauteur;
+ 	   }
+
+	    
+    	@Override
+    	public String toString() {
+    		StringBuilder espaceDeJeu = new StringBuilder();
+    		for (int y = 0; y < hauteur; y++) {
+    			for (int x = 0; x < longueur; x++) {
+    				if (vaisseau!=null && vaisseau.occupeLaPosition(x, y))
+    					espaceDeJeu.append('V');
+    				else
+    					espaceDeJeu.append('.');
+    			}
+    			espaceDeJeu.append('\n');
+    		}
+    		return espaceDeJeu.toString();
+    	}
+
+
+		public void positionnerUnNouveauVaisseau(int x, int y) {
+			Vaisseau vaisseau = new Vaisseau (x,y);
+			
 		}
-	   
-	   
 
    }
