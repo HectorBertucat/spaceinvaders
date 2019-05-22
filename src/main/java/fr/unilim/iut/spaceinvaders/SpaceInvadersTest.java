5075874ac043601ff840d155dd1360e12c870401
hectorbertucat <hectorbertucat@gmail.com>
2019-05-22 16:34:00 +0200

    Étape n°3 : Déplacer un vaisseau vers la droite en tenant compte de sa dimension

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
@@ -81,46 +81,6 @@ package fr.unilim.iut.spaceinvaders;
 		}
 
         @Test
-        public void test_VaisseauAvance_DeplacerVaisseauVersLaDroite() {
-
-            spaceinvaders.positionnerUnNouveauVaisseau(1,1,7,9);
-
-            spaceinvaders.deplacerVaisseauVersLaDroite();
-
-            assertEquals("" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "........V......\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
-        }
-
-        @Test
-        public void test_VaisseauImmobile_DeplacerVaisseauVersLaDroite() {
-
-            spaceinvaders.positionnerUnNouveauVaisseau(1,1,14,9);
-
-            spaceinvaders.deplacerVaisseauVersLaDroite();
-
-            assertEquals("" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "...............\n" +
-                    "..............V\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
-        }
-
-        @Test
         public void test_VaisseauAvance_DeplacerVaisseauVersLaGauche() {
 
             spaceinvaders.positionnerUnNouveauVaisseau(1,1,7,9);
@@ -172,6 +132,42 @@ package fr.unilim.iut.spaceinvaders;
 			} catch (final DebordementEspaceJeuException e) {
 			}
 
+		}
+
+		@Test
+		public void test_VaisseauAvance_DeplacerVaisseauVersLaDroite() {
+
+			spaceinvaders.positionnerUnNouveauVaisseau(3,2,7,9);
+			spaceinvaders.deplacerVaisseauVersLaDroite();
+			assertEquals("" +
+					"...............\n" +
+					"...............\n" +
+					"...............\n" +
+					"...............\n" +
+					"...............\n" +
+					"...............\n" +
+					"...............\n" +
+					"...............\n" +
+					"........VVV....\n" +
+					"........VVV....\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+		}
+
+		@Test
+		public void test_VaisseauImmobile_DeplacerVaisseauVersLaDroite() {
+
+			spaceinvaders.positionnerUnNouveauVaisseau(3,2,12,9);
+			spaceinvaders.deplacerVaisseauVersLaDroite();
+			assertEquals("" +
+					"...............\n" +
+					"...............\n" +
+					"...............\n" +
+					"...............\n" +
+					"...............\n" +
+					"...............\n" +
+					"...............\n" +
+					"...............\n" +
+					"............VVV\n" +
+					"............VVV\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
 		}
 
        }
